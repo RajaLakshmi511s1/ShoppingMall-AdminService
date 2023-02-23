f4817744ad9fa200b679d47ac4524f28eb075fc6
RajaLakshmi5111 <126055433+RajaLakshmi511s1@users.noreply.github.com>
2023-02-23 18:38:15 +0530

    Add files via upload

---
+++ Shoppingmall_AdminService/src/main/java/com/springproject/demo/ShoppingmallAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingmallAdminServiceApplication.class, args);
+	}
+
+}
